815c590ff35af73844f72cb25082bfec23e1bdcc
chmr1 <chmr1@hotmail.com>
2019-05-23 00:22:26 -0300

    Corrigindo o migration de usuários e sessão

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -6,8 +6,8 @@ const Schema = use('Schema')
 class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
-      table.increments()
-      table.string('username', 80).notNullable().unique()
+      table.bigIncrements()
+      table.string('username', 120).notNullable().unique()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
       table.timestamps()
