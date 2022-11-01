7aec60d528d100b993f7d2b30eac700dcc8f19e9
Carlos Henrique <chmr1@hotmail.com>
2022-11-01 08:54:22 -0300

    Ajuste para deploy

--- database/seeds/UserSeeder.js
+++ database/seeds/UserSeeder.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 /*
 |--------------------------------------------------------------------------
@@ -11,19 +11,54 @@
 */
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
-const Hash = use('Hash')
-const Database = use('Database')
+const Hash = use("Hash");
+const Database = use("Database");
 
 class UserSeeder {
-  static async run () {
-    await Database.table('users').insert([
-      { id: 1, username: 'admin', email: 'admin@myshelf.com', password: await Hash.make('123456'), created_at: Database.fn.now(), updated_at: Database.fn.now() },
-      { id: 2, username: 'demo', email: 'demo@myshelf.com', password: await Hash.make('123456'), created_at: Database.fn.now(), updated_at: Database.fn.now() },
-      { id: 3, username: 'fulano', email: 'fulano@myshelf.com', password: await Hash.make('123456'), created_at: Database.fn.now(), updated_at: Database.fn.now() },
-      { id: 4, username: 'Beltrano', email: 'beltrano@myshelf.com', password: await Hash.make('123456'), created_at: Database.fn.now(), updated_at: Database.fn.now() },
-      { id: 5, username: 'Sicrano', email: 'sicrano@myshelf.com', password: await Hash.make('123456'), created_at: Database.fn.now(), updated_at: Database.fn.now() }
-    ])
+  static async run() {
+    await Database.table("users").insert([
+      {
+        id: 1,
+        username: "admin",
+        email: "admin@myshelf.com",
+        password: await Hash.make("123456"),
+        created_at: Database.fn.now(),
+        updated_at: Database.fn.now(),
+      },
+      {
+        id: 2,
+        username: "demo",
+        email: "demo@myshelf.com",
+        password: await Hash.make("123456"),
+        created_at: Database.fn.now(),
+        updated_at: Database.fn.now(),
+      },
+      {
+        id: 3,
+        username: "fulano",
+        email: "fulano@myshelf.com",
+        password: await Hash.make("123456"),
+        created_at: Database.fn.now(),
+        updated_at: Database.fn.now(),
+      },
+      {
+        id: 4,
+        username: "Beltrano",
+        email: "beltrano@myshelf.com",
+        password: await Hash.make("123456"),
+        created_at: Database.fn.now(),
+        updated_at: Database.fn.now(),
+      },
+      {
+        id: 5,
+        username: "Sicrano",
+        email: "sicrano@myshelf.com",
+        password: await Hash.make("123456"),
+        created_at: Database.fn.now(),
+        updated_at: Database.fn.now(),
+      },
+    ]);
   }
 }
 
-module.exports = UserSeeder
+module.exports = UserSeeder;
